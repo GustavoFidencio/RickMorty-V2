dcbcaf3fc2cbe7e0e62624728063265f3a294dff
Gustavo Fidencio <gustavo@Gustavos-Mac-mini.local>
2022-07-16 15:52:42 -0300

    babel config basic arc

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          // '@constants': './src/constants',
+          // '@context': './src/context',
+          // '@factory': './src/factory',
+          '@components': './src/components',
+          '@pages': './src/pages',
+          '@route': './src/route',
+          '@services': './src/services',
+        }
+      }
+    ]
+  ]
 };
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, SafeAreaView } from 'react-native';
+
+export const Login = ({ }) => {
+
+    return (
+        <SafeAreaView
+            style={{
+                flex: 1,
+            }}
+        >
+            <Text style={{ backgroundColor: 'red' }}>
+                Teste
+            </Text>
+        </SafeAreaView>
+    )
+}
